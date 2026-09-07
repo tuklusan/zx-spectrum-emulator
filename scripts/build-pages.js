8e3c7f4a38b14e23d82a9661c9c1765856c115dc
Supratim Sanyal <tuklusan@users.noreply.github.com>
2026-09-07 14:27:25 -0400

    Update build script to copy new HTML files

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -4,7 +4,9 @@ import { copyFile, cp, mkdir, rm } from "node:fs/promises";
 await rm("dist", { recursive: true, force: true });
 await mkdir("dist", { recursive: true });
 
-await copyFile("public/index.html", "dist/index.html");
+//await copyFile("public/index.html", "dist/index.html");
+await copyFile("public/spectrum.html", "dist/index.html");
+await copyFile("public/index.html", "dist/machines.html");
 await copyFile("public/spectrum.html", "dist/spectrum.html");
 await copyFile("public/cpm.html", "dist/cpm.html");
 await copyFile("public/trs80.html", "dist/trs80.html");
